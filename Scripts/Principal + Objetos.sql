07b105206b1299ee399667b4b07d5f039fcaa381
andreloluciano <116128895+andreloluciano@users.noreply.github.com>
2023-12-17 15:41:20 -0300

    Update Scripts

--- Scripts/Principal + Objetos.sql
+++ Scripts/Principal + Objetos.sql
@@ -80,87 +80,6 @@ CREATE TABLE analisis_futbol_argentino (
     PRIMARY KEY (id_encuentro)
 );
 
--- Stored Procedures
-
--- Insertar Equipo Nuevo (primero escribir el nombre del equipo nuevo, luego el id locacion, esta locación debe ser una id existente)
-DELIMITER //
-CREATE PROCEDURE InsertarEquipo(
-    IN p_nombre_equipo VARCHAR(100),
-    IN p_id_locacion INT)
-BEGIN
-    INSERT INTO Equipos (equipo, id_locacion)
-    VALUES (p_nombre_equipo, p_id_locacion);
-END//
-
--- Para usar esta SP 
-CALL InsertarEquipo('Nuevo Equipo', 1);
-
-
--- Eliminar Equipo (solamente escribir el ID del equipo que queremos eliminar)
-DELIMITER //
-CREATE PROCEDURE EliminarEquipo(
-    IN p_id_equipo INT)
-BEGIN
-    DELETE FROM Equipos
-    WHERE id_equipo = p_id_equipo;
-END//
-
--- Para usar esta SP
-CALL EliminarEquipo('Id del equipo')
-
-
--- Actualizar datos de un Equipo, actualiza nombre y locacion de un equipo (insertar id equipo que queremos editar y actualizar el nombre e id locacion)
-DELIMITER //
-CREATE PROCEDURE ActualizarEquipo(
-    IN p_id_equipo INT,
-    IN p_nuevo_nombre_equipo VARCHAR(100),
-    IN p_nueva_locacion INT)
-BEGIN
-    UPDATE Equipos
-    SET equipo = p_nuevo_nombre_equipo, id_locacion = p_nueva_locacion
-    WHERE id_equipo = p_id_equipo;
-END//
-
--- Para usar esta SP
-CALL ActualizarEquipo('id equipo, nuevo nombre e id locacion')
-
--- Funciones
-
--- Esta función nos da el nombre del equipo según el ID que le demos
-DELIMITER //
-CREATE FUNCTION NombreEquipo(p_id_equipo INT)
-RETURNS VARCHAR(100)
-DETERMINISTIC
-BEGIN
-    DECLARE nombre_equipo VARCHAR(100);
-    SELECT equipo INTO nombre_equipo
-    FROM Equipos
-    WHERE id_equipo = p_id_equipo;
-    RETURN nombre_equipo;
-END;
-//
-
--- Para utilizarla debemos ingresar la siguiente query
-SELECT NombreEquipo(1);
-
--- Esta función obtiene la cantidad de partidos jugados por equipo según si ID
-
-DELIMITER //
-CREATE FUNCTION CantidadPartidosJugados(p_id_equipo INT)
-RETURNS INT
-DETERMINISTIC
-BEGIN
-    DECLARE cantidad_partidos INT;
-    SELECT COUNT(*) INTO cantidad_partidos
-    FROM analisis_futbol_argentino
-    WHERE id_equipo_local = p_id_equipo OR id_equipo_visitante = p_id_equipo;
-    RETURN cantidad_partidos;
-END;
-//
-
--- Para utilizar esta funcion debemos ingresar la siguiente query
-SELECT CantidadPartidosJugados(1);
-
 -- Vistas
 
 -- Esta vista une las tablas relevantes y selecciona campos clave para el análisis de resultados.
@@ -207,7 +126,8 @@ E.equipo AS Equipo
 FROM Locaciones L
 JOIN Equipos E ON L.id_locacion = E.id_locacion;
 
-SELECT * FROM LocacionesyEquipos;
+-- Para ver esta Vista:
+-- SELECT * FROM LocacionesyEquipos;
 
 -- Muestra una lista de equipos que participaron en cada campeonato durante el período de 2015-2022.
 CREATE VIEW CampeonatosyEquipos AS
@@ -216,10 +136,11 @@ E.equipo AS Equipo
 FROM Campeonatos C
 JOIN Equipos E ON C.id_campeonato = E.id_equipo;
 
-SELECT * FROM CampeonatosyEquipos;
+-- Para ver esta Vista
+-- SELECT * FROM CampeonatosyEquipos;
 
 -- Muestra un análisis de los resultados en la tabla principal
-CREATE VIEW Vista_Analisis_Resultados AS
+CREATE VIEW Vista_Analisis_Principal AS
 SELECT id_encuentro,
   goles_local,
   goles_visita,
@@ -229,7 +150,8 @@ SELECT id_encuentro,
   edad_media_visita
 FROM analisis_futbol_argentino;
 
-SELECT * FROM analisis_futbol_argentino;
+-- Para ver esta Vista
+-- SELECT * FROM analisis_futbol_principal;
 
 -- Muestra los equipos, dónde se llevaron a cabo y el resultado del partido, ordenado por fecha
 CREATE VIEW resultados_fecha AS
@@ -245,7 +167,8 @@ JOIN Locaciones L ON A.id_locacion = L.id_locacion
 JOIN Resultados R ON A.id_resultado = R.id_resultado
 ORDER BY A.fecha;
 
-SELECT *  FROM resultados_fecha
+-- Para ver esta Vista
+-- SELECT *  FROM resultados_fecha
 
 -- Muestra los partidos jugados el día domingo, que hayan resultado en empate y ordenados en fecha descendiente
 CREATE VIEW partidosdomingoempate AS
@@ -259,7 +182,91 @@ WHERE DAYOFWEEK(A.fecha) = 1
       AND A.id_resultado = 3  
 ORDER BY A.fecha DESC;
 
-SELECT *  FROM partidosdomingoempate
+-- Para ver esta Vista
+-- SELECT *  FROM partidosdomingoempate
+
+-- Stored Procedures
+
+-- Insertar Equipo Nuevo (primero escribir el nombre del equipo nuevo, luego el id locacion, esta locación debe ser una id existente)
+DELIMITER //
+CREATE PROCEDURE InsertarEquipo(
+    IN p_nombre_equipo VARCHAR(100),
+    IN p_id_locacion INT)
+BEGIN
+    INSERT INTO Equipos (equipo, id_locacion)
+    VALUES (p_nombre_equipo, p_id_locacion);
+END//
+
+-- Para usar esta SP 
+-- CALL InsertarEquipo('Nuevo Equipo', 1);
+
+
+-- Eliminar Equipo (solamente escribir el ID del equipo que queremos eliminar)
+DELIMITER //
+CREATE PROCEDURE EliminarEquipo(
+    IN p_id_equipo INT)
+BEGIN
+    DELETE FROM Equipos
+    WHERE id_equipo = p_id_equipo;
+END//
+
+-- Para usar esta SP
+-- CALL EliminarEquipo('Id del equipo')
+
+
+-- Actualizar datos de un Equipo, actualiza nombre y locacion de un equipo (insertar id equipo que queremos editar y actualizar el nombre e id locacion)
+DELIMITER //
+CREATE PROCEDURE ActualizarEquipo(
+    IN p_id_equipo INT,
+    IN p_nuevo_nombre_equipo VARCHAR(100),
+    IN p_nueva_locacion INT)
+BEGIN
+    UPDATE Equipos
+    SET equipo = p_nuevo_nombre_equipo, id_locacion = p_nueva_locacion
+    WHERE id_equipo = p_id_equipo;
+END//
+
+-- Para usar esta SP
+-- CALL ActualizarEquipo('id equipo, nuevo nombre e id locacion')
+
+-- Funciones
+
+-- Esta función nos da el nombre del equipo según el ID que le demos
+DELIMITER //
+CREATE FUNCTION NombreEquipo(p_id_equipo INT)
+RETURNS VARCHAR(100)
+DETERMINISTIC
+BEGIN
+    DECLARE nombre_equipo VARCHAR(100);
+    SELECT equipo INTO nombre_equipo
+    FROM Equipos
+    WHERE id_equipo = p_id_equipo;
+    RETURN nombre_equipo;
+END;
+//
+
+-- Para utilizarla debemos ingresar la siguiente query
+-- SELECT NombreEquipo(1);
+
+-- Esta función obtiene la cantidad de partidos jugados por equipo según si ID
+
+DELIMITER //
+CREATE FUNCTION CantidadPartidosJugados(p_id_equipo INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE cantidad_partidos INT;
+    SELECT COUNT(*) INTO cantidad_partidos
+    FROM analisis_futbol_argentino
+    WHERE id_equipo_local = p_id_equipo OR id_equipo_visitante = p_id_equipo;
+    RETURN cantidad_partidos;
+END;
+//
+
+-- Para utilizar esta funcion debemos ingresar la siguiente query
+-- SELECT CantidadPartidosJugados(1);
+
+
 
 -- Triggers
 
